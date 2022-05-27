4eec5af4d5efd7fdcf378fd2f2efd273941f03c7
hannesprojects <106339612+hannesprojects@users.noreply.github.com>
2022-05-27 16:11:24 +0200

    Add files via upload

---
+++ learning_R_diagramm_Farbe.R
@@ -0,0 +1,14 @@
+HairEyeData<-HED
+library(ggplot2)
+----------------------------------------------------------
+  #Balkendiagramm erstellen und Legende
+  ggplot(data = HairEyeData, aes(x=Sex,fill=Sex)) +
+  #Rahmenfarbe zuordnen
+  geom_bar(color="black") +
+  #Einzelne Farben den Balen zuordnen
+  scale_fill_manual(values = c("#FF6666","#CCFF66"))+
+  #Achsenbeschriftung und Überschrift umbennen
+  labs(y="Anzahl",
+       x="Geschlecht",
+       title="Häufigkeitstabelle")
+----------------------------------------------------------
